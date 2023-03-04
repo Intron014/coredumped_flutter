227ac89fb52791ad4e12355d4186835425579056
Jorge Benjumea Burillo <me@intron014.com>
2023-03-04 19:43:05 +0100

    There's more to it than I thought :)

--- lib/main.dart
+++ lib/main.dart
@@ -1,178 +1,216 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
-
-
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'The Almighty App  ',
+      title: 'Fridge',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Homie Page'),
+      home: MyHomePage(),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  _MyHomePageState createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+  int _selectedIndex = 0;
 
-  void _incrementCounter() {
+  void _onNavItemTapped(int index) {
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+      _selectedIndex = index;
     });
+  }
+
+  void _onButton1Pressed() {
+    Navigator.push(
+      context,
+      MaterialPageRoute(builder: (context) => Page2()),
+    );
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        //Put an icon at the top right
-        actions: <Widget>[
-          IconButton(
-            icon: const Icon(Icons.account_circle),
-            tooltip: 'Profile',
-            onPressed: () {
-              setState(() {
-                _counter = 0;
-              });
-            },
-          ),
-        ],
-        title: Text('The Almighty Fridge'),
-        centerTitle: true,
-      ),
-      //Make a button that resets counter
-
-      drawer: Drawer(
-        child: ListView(
-          padding: EdgeInsets.zero,
-          children: const <Widget>[
-            DrawerHeader(
-              decoration: BoxDecoration(
-                color: Colors.blue,
-              ),
-              child: Text(
-                'Menu',
-                style: TextStyle(
-                  color: Colors.white,
-                  fontSize: 24,
+    final List<Widget> _mainPages = [
+      Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              'POTATO',
+              style: TextStyle(fontSize: 32),
+            ),
+            SizedBox(height: 8),
+            Icon(
+              Icons.mail,
+              color: Colors.blueGrey,
+              size: 60.0,
+            ),
+            SizedBox(height: 24),
+            Card(
+              child: TextButton(
+                onPressed: _onButton1Pressed,
+                child: ListTile(
+                  leading: Icon(Icons.ac_unit),
+                  title: Center(child: Text('Button 1')),
+                  trailing: Icon(Icons.arrow_forward_ios),
+                ),
+                style: TextButton.styleFrom(
+                  padding: EdgeInsets.symmetric(horizontal: 24, vertical: 16),
+                  minimumSize: Size(double.infinity, 0),
                 ),
               ),
             ),
-            ListTile(
-              leading: Icon(Icons.message),
-              title: Text('Messages'),
+            SizedBox(height: 8),
+            Card(
+              child: TextButton(
+                onPressed: () {},
+                child: ListTile(
+                  leading: Icon(Icons.beach_access),
+                  title: Center(child: Text('Button 2')),
+                  trailing: Icon(Icons.arrow_forward_ios),
+                ),
+                style: TextButton.styleFrom(
+                  padding: EdgeInsets.symmetric(horizontal: 24, vertical: 16),
+                  minimumSize: Size(double.infinity, 0),
+                ),
+              ),
             ),
-            ListTile(//reset counter when c/take me to the profile pagelicked
-              leading: Icon(Icons.account_circle),
-              title: Text('Profile'),
-
-            ),
-
-            ListTile(
-              leading: Icon(Icons.settings),
-              title: Text('Settings'),
+            SizedBox(height: 8),
+            Card(
+              child: TextButton(
+                onPressed: () {},
+                child: ListTile(
+                  leading: Icon(Icons.cloud),
+                  title: Center(child: Text('Button 3')),
+                  trailing: Icon(Icons.arrow_forward_ios),
+                ),
+                style: TextButton.styleFrom(
+                  padding: EdgeInsets.symmetric(horizontal: 24, vertical: 16),
+                  minimumSize: Size(double.infinity, 0),
+                ),
+              ),
             ),
           ],
         ),
+      ),
+    ];
+
+    final List<Widget> _sidebarItems = [
+      ListTile(
+        leading: Icon(Icons.home),
+        title: Text('Home'),
+        onTap: () {
+          _onNavItemTapped(0);
+          Navigator.pop(context);
+        },
+      ),
+      ListTile(
+        leading: Icon(Icons.settings),
+        title: Text('Settings'),
+        onTap: () {
+          _onNavItemTapped(1);
+          Navigator.pop(context);
+        },
+      ),
+      ListTile(
+        leading: Icon(Icons.info),
+        title: Text('About'),
+        onTap: () {
+          _onNavItemTapped(2);
+          Navigator.pop(context);
+        },
+      ),
+      ListTile(
+        leading: Icon(Icons.help),
+        title: Text('Help'),
+        onTap: () {
+          _onNavItemTapped(3);
+          Navigator.pop(context);
+        },
+      ),
+    ];
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Fridge'),
+      ),
+      body: _mainPages[_selectedIndex],
+      drawer: Drawer(
+        child: ListView(
+          children: [
+            DrawerHeader(
+              child: Text('Fridge'),
+              decoration: BoxDecoration(
+                color: Colors.blue,
+              ),
+            ),
+            ..._sidebarItems,
+          ],
+        ),
+      ),
+        floatingActionButtonLocation: FloatingActionButtonLocation.endFloat,
+        floatingActionButton: Column(
+    mainAxisAlignment: MainAxisAlignment.end,
+    crossAxisAlignment: CrossAxisAlignment.end,
+    children: [
+
+    FloatingActionButton(
+    onPressed: () {},
+    tooltip: 'Button 1',
+    child: Icon(Icons.ac_unit),
+    ),
+    ],
+    ),
+    );
+  }
+}
+
+class Page2 extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final List<Widget> _buttons = [
+    ElevatedButton(
+      onPressed: () {},
+      child: Text('Button 1'),
+      style: ElevatedButton.styleFrom(
+        primary: Colors.red,
+      ),
+    ),
+    ElevatedButton(
+    onPressed: () {},
+    child: Text('Button 2'),
+    style: ElevatedButton.styleFrom(
+    primary: Colors.orange,
+    ),
+    ),
+    ElevatedButton(
+    onPressed: () {},
+    child: Text('Button 3'),
+    style: ElevatedButton.styleFrom(
+    primary: Colors.yellow,
+    ))
+    ];
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Page 2'),
       ),
       body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
           mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Icon(Icons.mail,
-                size: 100,
-                color: _counter%2==0?Colors.red:Colors.blue),
-            if(_counter==5) const Text(
-              style: TextStyle(
-                fontFamily: 'Courier',
-                fontSize: 20,
-                fontWeight: FontWeight.bold,
-                color: Colors.red,
-              ),
-              'You have clicked 5 times',
-
-            ),
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
+          children: _buttons,
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
